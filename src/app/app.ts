3d9219f5a925576506f1a0299ffe0c42d54ec65e
chris <9386112+cr15p@users.noreply.github.com>
2025-06-10 11:28:11 +0200

    [CHG] change basic files to make prototype version of frontend app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskList } from "./task-list/task-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
